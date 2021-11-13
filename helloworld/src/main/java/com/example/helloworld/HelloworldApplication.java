9ab987655282b219fe33cbdc9a322fe43c048c30
amos-cha <78386128+amos-cha@users.noreply.github.com>
2021-11-13 00:55:23 -0500

    Update HelloworldApplication.java

--- helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -16,7 +16,7 @@ public class HelloworldApplication {
   class HelloworldController {
     @GetMapping("/")
     String hello() {
-      return "Hello " + name + "!";
+      return "Hello " + name + "!" + " this is a test?";
     }
   }
 
