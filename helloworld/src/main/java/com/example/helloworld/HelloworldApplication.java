3d364d238e2d8846346776963e58a00a9f65e2b0
amos-cha <78386128+amos-cha@users.noreply.github.com>
2021-11-13 22:44:57 -0500

    Update HelloworldApplication.java

--- helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,13 +15,24 @@ public class HelloworldApplication {
 
   @Value("${NAME:World}")
   String name;
+  
 
   @RestController
   class HelloworldController {
-    @GetMapping("/")
-    String hello() {
-      return "testing again";
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    String getFoosBySimplePathWithPathVariable(@PathVariable("id") String id) {
+        return id;
     }
+    // String hello() {
+    //   return "testing again";
+    // }
+
+    
+    // @GetMapping("/")
+    // String hello() {
+    //   return "testing again";
+    // }
   }
 
   public static void main(String[] args) {
